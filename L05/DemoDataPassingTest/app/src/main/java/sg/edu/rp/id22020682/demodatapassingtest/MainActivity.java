58b70d171436b7362be9b3d3edf62d00c60757a4
AdleeAqeel <m.adlee.aqeel@gmail.com>
2023-05-22 14:23:47 +0800

    ini

---
+++ L05/DemoDataPassingTest/app/src/main/java/sg/edu/rp/id22020682/demodatapassingtest/MainActivity.java
@@ -0,0 +1,42 @@
+package sg.edu.rp.id22020682.demodatapassingtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnPassInteger;
+    Button btnCharInterger;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnPassInteger = findViewById(R.id.buttonPassInteger);
+        btnCharInterger = findViewById(R.id.buttonPassChar);
+
+
+        btnPassInteger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("value", 1);
+                startActivity(intent);
+            }
+        });
+        btnCharInterger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ThirdActivity.class);
+                intent.putExtra("value", "a");
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}

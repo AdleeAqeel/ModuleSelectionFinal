4ba14bddb2152f6a2e7c9b66e146a38a347f0a5a
AdleeAqeel <123053542+AdleeAqeel@users.noreply.github.com>
2023-06-05 11:50:32 +0800

    ini

---
+++ L07/DemoSpinnerTest/app/src/main/java/sg/edu/rp/id22020682/demospinnertest/MainActivity.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.id22020682.demospinnertest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
